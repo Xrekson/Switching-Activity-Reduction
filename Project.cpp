1001dd9fbecfb8855f193ef9b2ce6fd5277d9d58
Indra <indraneel1080brahma@gmail.com>
2023-01-31 13:51:47 +0530

    Gates in place

--- Project.cpp
+++ Project.cpp
@@ -1,18 +1,65 @@
 #include<iostream>
 #include<string>
 
-int main() {
-    float x,y;
-    x=y=0.5;
-    std::string expression;
-    std::cout<<"Give the Expression:"<<std::endl;
-    std::cin>>expression;
-    size_t gate = expression.find("+");
+float OR(std::string expression,float x,float y){
+    size_t gate = expression.find("OR");
     if(gate == 0){
         std::cout<<"Unspecified GATE Expresion";
     }else {
         float p=(x+y-x*y);
         std::cout<<"Switching Activity is "<<p<<std::endl;
-        return 0;
+        return p;
     }
+};
+
+float AND(std::string expression,float x,float y){
+    size_t gate = expression.find("AND");
+    if(gate == 0){
+        std::cout<<"Unspecified GATE Expresion";
+    }else {
+        float p=(x*y);
+        std::cout<<"Switching Activity is "<<p<<std::endl;
+        return p;
+    }
+};
+
+float NAND(std::string expression,float x,float y){
+    size_t gate = expression.find("NAND");
+    if(gate == 0){
+        std::cout<<"Unspecified GATE Expresion";
+    }else {
+        float p=(1-x*y);
+        std::cout<<"Switching Activity is "<<p<<std::endl;
+        return p;
+    }
+};
+
+float NOR(std::string expression,float x,float y){
+    size_t gate = expression.find("NOR");
+    if(gate == 0){
+        std::cout<<"Unspecified GATE Expresion";
+    }else {
+        float p;
+        p=((1-x)*(1-y));
+        std::cout<<"Switching Activity is "<<p<<std::endl;
+        return p;
+    }
+};
+
+float NOT(std::string expression,float x){
+    size_t gate = expression.find("NOT");
+    if(gate == 0){
+        std::cout<<"Unspecified GATE Expresion";
+    }else {
+        float p=(1-x);
+        std::cout<<"Switching Activity is "<<p<<std::endl;
+        return p;
+    }
+};
+
+std::string parser(std::string exp){
+    
+} 
+
+int main() {
 };
