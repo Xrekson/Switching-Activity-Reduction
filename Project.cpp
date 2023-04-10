32a5f607fb350225bd720ac3f1b74012042eaa70
Indraneel Brahma <76116355+Xrekson@users.noreply.github.com>
2023-04-10 12:06:23 +0530

    Choice Update

--- Project.cpp
+++ Project.cpp
@@ -1,65 +1,92 @@
 #include<iostream>
 #include<string>
 
-float OR(std::string expression,float x,float y){
-    size_t gate = expression.find("OR");
-    if(gate == 0){
-        std::cout<<"Unspecified GATE Expresion";
-    }else {
+float OR(float x,float y){
+    {
         float p=(x+y-x*y);
-        std::cout<<"Switching Activity is "<<p<<std::endl;
+		float sa = p*(1-p);
+		std::cout<<"Probability is "<<p<<std::endl;
+        std::cout<<"Switching Activity is "<<sa<<std::endl;
         return p;
     }
 };
 
-float AND(std::string expression,float x,float y){
-    size_t gate = expression.find("AND");
-    if(gate == 0){
-        std::cout<<"Unspecified GATE Expresion";
-    }else {
+float AND(float x,float y){
+    {
         float p=(x*y);
-        std::cout<<"Switching Activity is "<<p<<std::endl;
-        return p;
+		float sa = p*(1-p);
+		std::cout<<"Probability is "<<p<<std::endl;
+        std::cout<<"Switching Activity is "<<sa<<std::endl;
+        return sa;
     }
 };
 
-float NAND(std::string expression,float x,float y){
-    size_t gate = expression.find("NAND");
-    if(gate == 0){
-        std::cout<<"Unspecified GATE Expresion";
-    }else {
+float NAND(float x,float y){
+    {
         float p=(1-x*y);
-        std::cout<<"Switching Activity is "<<p<<std::endl;
-        return p;
+		float sa = p*(1-p);
+		std::cout<<"Probability is "<<p<<std::endl;
+        std::cout<<"Switching Activity is "<<sa<<std::endl;
+        return sa;
     }
 };
 
-float NOR(std::string expression,float x,float y){
-    size_t gate = expression.find("NOR");
-    if(gate == 0){
-        std::cout<<"Unspecified GATE Expresion";
-    }else {
+float NOR(float x,float y){
+    {
         float p;
         p=((1-x)*(1-y));
-        std::cout<<"Switching Activity is "<<p<<std::endl;
-        return p;
+		float sa = p*(1-p);
+		std::cout<<"Probability is "<<p<<std::endl;
+        std::cout<<"Switching Activity is "<<sa<<std::endl;
+        return sa;
     }
 };
 
-float NOT(std::string expression,float x){
-    size_t gate = expression.find("NOT");
-    if(gate == 0){
-        std::cout<<"Unspecified GATE Expresion";
-    }else {
+float NOT(float x){
+    {
         float p=(1-x);
-        std::cout<<"Switching Activity is "<<p<<std::endl;
-        return p;
+		float sa = p*(1-p);
+		std::cout<<"Probability is "<<p<<std::endl;
+        std::cout<<"Switching Activity is "<<sa<<std::endl;
+        return sa;
     }
 };
 
-std::string parser(std::string exp){
-    
-} 
 
 int main() {
+	int choise;
+	float x,y;
+	std::cout<<"Gate choise:";
+	std::cout<<"1-AND, 2-OR, 3-NAND, 4-NOR, 5-NOT"<<std::endl;
+	std::cin>>choise;
+	do{
+	if(choise == 1){
+		std::cout<<"Input Probability:"<<std::endl;
+		std::cout<<"X:";std::cin>>x;
+		std::cout<<"Y:";std::cin>>y;
+		AND(x,y);
+	}else if(choise == 2){
+		std::cout<<"Input Probability:"<<std::endl;
+		std::cout<<"X:";std::cin>>x;
+		std::cout<<"Y:";std::cin>>y;
+		OR(x,y);
+	} else if(choise == 3){
+		std::cout<<"Input Probability:"<<std::endl;
+		std::cout<<"X:";std::cin>>x;
+		std::cout<<"Y:";std::cin>>y;
+		NAND(x,y);
+	}else if(choise == 4){
+		std::cout<<"Input Probability:"<<std::endl;
+		std::cout<<"X:";std::cin>>x;
+		std::cout<<"Y:";std::cin>>y;
+		NOR(x,y);
+	}else if(choise == 5){
+		std::cout<<"Input Probability:"<<std::endl;
+		std::cout<<"X:";std::cin>>x;
+		NOT(x);
+	}
+	std::cout<<"1-AND, 2-OR, 3-NAND, 4-NOR, 5-NOT"<<std::endl;
+	std::cin>>choise;
+	}
+	while(choise!=0);
 };
