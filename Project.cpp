d24add7f5d062f1efc32b51810786f14f6bb3c0f
Indra <indraneel1080brahma@gmail.com>
2023-06-08 11:15:24 +0530

    Got all probabilities

--- Project.cpp
+++ Project.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <vector>
 
+std::vector<std::vector<std::vector<long double>>> reden;
 long double OR(long double x,long double y){
     {
         long double p=(x+y-x*y);
@@ -40,6 +41,100 @@ long double NOT(long double x){
         return p;
     }
 };
+std::vector<long double> branchAND(std::vector<long double> inden){
+    auto n = inden.begin();
+    long double p1,p2,a,b;
+    std::vector<long double> result;
+    while(n!=inden.end()){
+        if(n==inden.begin()){
+            p1=*n;
+            n++;
+        }else if(*(n-1)==p1 || p1==AND(a,b)){
+            p2=*n;
+            if(p1<0){
+                a=p1*(-1);
+            }else{
+                a=p1;
+            }
+            if(p2<0){
+                b=p2*(-1);
+            }else{
+                b=p2;
+            }
+            p1=AND(a,b);
+            result.push_back(p1);
+            n++;
+        }
+    }
+    return result;
+};
+
+std::vector<long double> branchOR(std::vector<long double> inden){
+    auto n = inden.begin();
+    long double p1,p2,a,b;
+    std::vector<long double> result;
+    while(n!=inden.end()){
+        if(n==inden.begin()){
+            p1=*n;
+            n++;
+        }else if(*(n-1)==p1 || p1==OR(a,b)){
+            p2=*n;
+            a=p1;
+            b=p2;
+            p1=OR(a,b);
+            result.push_back(p1);
+            n++;
+        }
+    }
+    return result;
+};
+
+void SA(std::vector<std::vector<std::vector<long double>>> list){
+    std::vector<std::vector<std::vector<long double>>> ult;
+    std::vector<std::vector<long double>> result;
+    std::vector<long double> base;
+    int r=0;
+    for(int i=0;i<list.size();i++)
+    {
+        for (int j=0;j<list[i].size();j++){
+            result.push_back(branchAND(list[i][j]));
+        }
+        reden.push_back(result);
+        ult.push_back(result);
+        result.clear();
+    }
+    for(int i=0;i<ult.size();i++)
+    {
+        for (int j=0;j<ult[i].size();j++){
+            base.push_back(ult[i][j][ult[i][j].size()-1]);
+        }
+        reden[i].push_back(base);
+        result.push_back(branchOR(base));
+        base.clear();
+    }
+    for(int i=0;i<result.size();i++)
+    {
+        if(result[i].size()>1){
+        do{
+            base=branchOR(result[i]);
+            for(auto j = base.begin();j!=base.end();j++){
+                std::cout<<*j<<" ";
+            }
+            printf("\n");
+            result[i]=base;
+            reden[i].push_back(base);
+        }while(result[i].size()!=1);
+    }
+    }
+    for(int i=0;i<result.size();i++){
+        std::cout<<"Switching-Activity for f"<<i<<":";
+        for(auto k = result[i].begin(); k != result[i].end();k++){
+                std::cout<< *k <<" ";
+            }
+            printf("\n");
+    }
+    
+};
 
 int main() 
 {
@@ -47,8 +142,9 @@ int main()
     std::string inputfile,outputfile;
     std::string line;
     std::vector<int> number;
-    std::vector<std::vector<int>> drain;
-    std::vector<std::vector<std::vector<int>>> merge;
+    std::vector<long double> bane;
+    std::vector<std::vector<long double>> drain;
+    std::vector<std::vector<std::vector<long double>>> merge;
     std::vector<std::string> linesin,linesout;
     int InputNum=0;
     inFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
@@ -77,7 +173,7 @@ int main()
         }
     }
     std::vector<std::vector<int>> output1;
-    for(int n=0;n<linesout[0].length()-1;n++){
+    for(int n=0;n<linesout[0].length();n++){
         for(int i=0;i<linesout.size();i++){
             if(linesout[i][n]=='1'){
                 number.push_back(i);
@@ -92,18 +188,17 @@ int main()
         for (auto j = output1[i].begin(); j != output1[i].end(); ++j)
         std::cout << *j << " " << std::endl;
     }
-    std::vector<std::vector<int>> numbers;
+    std::vector<std::vector<long double>> numbers;
     for(int i = 0;i<linesin.size();i++){
         for(int j=0;j<linesin[0].length();j++){
             if(linesin[i][j]=='1'){
-                number.push_back(j);
+                bane.push_back(0.5l);
             }else if(linesin[i][j]=='0'){
-                int l=0-j;
-                number.push_back(l);
+                bane.push_back(-0.5l);
             }
         }
-        numbers.push_back(number);
-        number.clear();
+        numbers.push_back(bane);
+        bane.clear();
     }
     for(int i=0;i<numbers.size();i++)
     {
@@ -133,42 +228,15 @@ int main()
         std::cout<<""<<std::endl;
         }
     }
-
+    SA(merge);
+    for(int i=0;i<reden.size();i++)
+    {
+        std::cout<<"Probability for f"<< i <<std::endl;
+        for (int j=0;j<reden[i].size();j++){
+            for(auto k = reden[i][j].begin(); k != reden[i][j].end();k++){
+                std::cout<< *k <<" ";
+            }
+        }
+        printf("\n");
+    }
 };
-/* int main() {
-	int choise;
-	long double x,y;
-	std::cout<<"Gate choise:";
-	std::cout<<"1-AND, 2-OR, 3-NAND, 4-NOR, 5-NOT"<<std::endl;
-	std::cin>>choise;
-	do{
-	if(choise == 1){
-		std::cout<<"Input Probability:"<<std::endl;
-		std::cout<<"X:";std::cin>>x;
-		std::cout<<"Y:";std::cin>>y;
-		AND(x,y);
-	}else if(choise == 2){
-		std::cout<<"Input Probability:"<<std::endl;
-		std::cout<<"X:";std::cin>>x;
-		std::cout<<"Y:";std::cin>>y;
-		OR(x,y);
-	} else if(choise == 3){
-		std::cout<<"Input Probability:"<<std::endl;
-		std::cout<<"X:";std::cin>>x;
-		std::cout<<"Y:";std::cin>>y;
-		NAND(x,y);
-	}else if(choise == 4){
-		std::cout<<"Input Probability:"<<std::endl;
-		std::cout<<"X:";std::cin>>x;
-		std::cout<<"Y:";std::cin>>y;
-		NOR(x,y);
-	}else if(choise == 5){
-		std::cout<<"Input Probability:"<<std::endl;
-		std::cout<<"X:";std::cin>>x;
-		NOT(x);
-	}
-	std::cout<<"1-AND, 2-OR, 3-NAND, 4-NOR, 5-NOT"<<std::endl;
-	std::cin>>choise;
-	}
-	while(choise!=0);
-}; */
